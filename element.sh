564265fb16147dd70627ec753b35e7229d094d49
MPhuong1499 <phuongdoan1499@gmail.com>
2025-03-21 10:07:09 +0000

    test:failed

--- element.sh
+++ element.sh
@@ -1,8 +1,14 @@
 #! /bin/bash
 
+PSQL="psql -X --username=freecodecamp --dbname=periodic_table --tuples-only -c"
+
 if [[ -z "$1" ]]; then
     echo "Please provide an element as an argument."
     exit 1
 else
-    echo "$1"
+    if [[ "$1" =~ ^[0-9]+$ || "$1" -gt 0 || "$1" -lt 11 ]]; then 
+    echo right
+    exit 1
+    else echo wrong
+    fi
 fi
