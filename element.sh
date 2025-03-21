ef7fa3267bffcb3e51ae8c62f1e0541d8d9c4c1c
MPhuong1499 <phuongdoan1499@gmail.com>
2025-03-21 10:56:23 +0000

    test: attempt 2

--- element.sh
+++ element.sh
@@ -17,7 +17,6 @@ if [[ "$INPUT" =~ ^[0-9]+$ ]]; then
     # Query by atomic_number
     RESULT=$($PSQL "SELECT e.atomic_number, e.name, e.symbol, t.type, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius FROM elements e JOIN properties p ON e.atomic_number = p.atomic_number JOIN types t ON p.type_id = t.type_id WHERE e.atomic_number = $INPUT;")
     read RESULT NAME SYMBOL TYPE MASS MELTING BOILING <<< $(echo "$RESULT" | tr '|' ' ' | xargs)
-    
 else
     # Query by symbol or name
     RESULT=$($PSQL "SELECT e.atomic_number, e.name, e.symbol, t.type, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius FROM elements e JOIN properties p ON e.atomic_number = p.atomic_number JOIN types t ON p.type_id = t.type_id WHERE symbol = '$INPUT' OR name = '$INPUT';")
@@ -31,4 +30,4 @@ if [[ -z "$RESULT" ]]; then
 fi
 
 # Output the atomic_number
-echo "The element with atomic number $RESULT is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING celsius and a boiling point of $BOILING celsius." 
+echo "The element with atomic number $RESULT is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING celsius and a boiling point of $BOILING celsius."
